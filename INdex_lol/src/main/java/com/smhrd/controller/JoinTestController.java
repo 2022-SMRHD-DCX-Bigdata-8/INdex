04d93b64114831e24b90c9ece85cf9bb48a4c71b
smhrd <smhrd@DESKTOP-SN783QO>
2023-08-16 15:48:49 +0900

    개인api 실시간 연동 후 db 저장 로직 큰산을 넘었습니다

--- INdex_lol/src/main/java/com/smhrd/controller/JoinTestController.java
+++ INdex_lol/src/main/java/com/smhrd/controller/JoinTestController.java
@@ -16,6 +16,8 @@ public class JoinTestController implements L_Controller {
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		String email = request.getParameter("email");
+		String name = request.getParameter("name");
+		String tel = request.getParameter("tel");
 		String pw = request.getParameter("password");
 		String lolNick = request.getParameter("lolNickname");
 		L_user member = new L_user();
@@ -24,13 +26,20 @@ public class JoinTestController implements L_Controller {
 		
 
 		try {
-			// API 호출 및 고유 id 찾아오는 로직
-			String puuid = ApiUtils.getLolpuuid(lolNick);
 			member.setU_EMAIL(email);
 			member.setU_PW(pw);
 			member.setU_NICK(lolNick);
+			member.setU_TEL(tel);
+			member.setU_NAME(name);
+			
+			System.out.printf("Email: %s, 비밀번호: %s, LoL 닉네임: %s\n", email, pw, lolNick);
+			// API 호출 및 고유 id 찾아오는 로직
+			String puuid = ApiUtils.getLolpuuid(lolNick);
 			member.setU_LOLCD(puuid);
-			member = new L_user(email,pw,lolNick,puuid);
+			System.out.println(puuid);
+			
+			member = new L_user(email,pw,lolNick,puuid,name,tel);
+			System.out.println(member);
 
 			// 데이터베이스에 저장
 			int cnt = dao.join(member);
--- INdex_lol/src/main/webapp/WEB-INF/view/main.jsp
+++ INdex_lol/src/main/webapp/WEB-INF/view/main.jsp
@@ -8,14 +8,14 @@
 </head>
 <body>
 	<form action="Jointest.do" method="post">
-		<label for="email">Email:</label> <input type="email" id="email"
-			name="email" required><br> 
-		<label for="password">비밀번호:</label>
-		<input type="password" id="password" name="password" required><br>
+		이름<input type="text" id="email" name="name" required><br>
+		핸드폰번호<input type="tel" id="email"
+			name="tel" required><br> 
+		Email:<input type="text" id="email" name="email" required><br>
+		비밀번호: <input type="password" id="password" name="password" required><br>
 
-		<label for="lolNickname">LoL 닉네임:</label> <input type="text"
-			id="lolNickname" name="lolNickname" required><br> <input
-			type="submit" value="회원가입">
+		LoL 닉네임:<input type="text" id="lolNickname" name="lolNickname"
+			required><br> <input type="submit" value="회원가입">
 	</form>
 	<script src="assets/js/jquery.min.js"></script>
 	<script src="assets/js/jquery.scrolly.min.js"></script>
