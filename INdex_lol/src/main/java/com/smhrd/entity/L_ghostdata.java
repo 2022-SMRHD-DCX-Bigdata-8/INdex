24922f90f055ea99bd80507b17222fab6508b362
smhrd <smhrd@DESKTOP-SN783QO>
2023-08-21 15:53:31 +0900

    DB랑 컬럼맞춤

--- INdex_lol/src/main/java/com/smhrd/entity/L_ghostdata.java
+++ INdex_lol/src/main/java/com/smhrd/entity/L_ghostdata.java
@@ -12,8 +12,14 @@ public class L_ghostdata {
 	private String g_rank;
 	private int g_gold;
 	private int g_level ;
-	private String g_kda;
+	private int g_kill;
+	private int g_death;
+	private int g_assist;
 	private int g_damage;
 	private int g_playtime;
-	private int g_sigthscore;
+	private int g_wardskill;
+	private int g_wardsplaced;
+	private int g_minionkill;
+	private int g_cs;
+	private String g_teamposition;
 }
